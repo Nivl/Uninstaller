cb58f07e39ed20e4a3b560037df7e542d929c52c
Melvin Laplanche <melvin.wont.reply@gmail.com>
2020-11-29 15:56:55 -0800

    Initial Commit

---
+++ Uninstaller/UninstallerApp.swift
@@ -0,0 +1,17 @@
+//
+//  UninstallerApp.swift
+//  Uninstaller
+//
+//  Created by Melvin Laplanche on 11/29/20.
+//
+
+import SwiftUI
+
+@main
+struct UninstallerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
